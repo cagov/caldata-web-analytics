734a654c629603a5c29b66e5fd1e2354a2910a61
Ian Rose <ian.rose@innovation.ca.gov>
2023-10-11 13:23:39 -0700

    Add prd configuration

---
+++ terraform/environments/prd/main.tf
@@ -0,0 +1,74 @@
+############################
+#         Variables        #
+############################
+
+variable "environment" {
+  description = "Environment suffix"
+  type        = string
+}
+
+variable "locator" {
+  description = "Snowflake account locator"
+  type        = string
+}
+
+############################
+#         Providers        #
+############################
+
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.69"
+    }
+  }
+  required_version = ">= 1.0"
+  backend "s3" {
+  }
+}
+
+# This provider is intentionally low-permission. In Snowflake, object creators are
+# the default owners of the object. To control the owner, we create different provider
+# blocks with different roles, and require that all snowflake resources explicitly
+# flag the role they want for the creator.
+provider "snowflake" {
+  account = var.locator
+  role    = "PUBLIC"
+}
+
+# Snowflake provider for creating databases, warehouses, etc.
+provider "snowflake" {
+  alias   = "sysadmin"
+  account = var.locator
+  role    = "SYSADMIN"
+}
+
+# Snowflake provider for managing grants to roles.
+provider "snowflake" {
+  alias   = "securityadmin"
+  account = var.locator
+  role    = "SECURITYADMIN"
+}
+
+# Snowflake provider for managing user accounts and roles.
+provider "snowflake" {
+  alias   = "useradmin"
+  account = var.locator
+  role    = "USERADMIN"
+}
+
+############################
+#       Environment        #
+############################
+
+module "elt" {
+  source = "github.com/cagov/data-infrastructure.git//terraform/snowflake/modules/elt?ref=2b4ac50"
+  providers = {
+    snowflake.securityadmin = snowflake.securityadmin,
+    snowflake.sysadmin      = snowflake.sysadmin,
+    snowflake.useradmin     = snowflake.useradmin,
+  }
+
+  environment = var.environment
+}
