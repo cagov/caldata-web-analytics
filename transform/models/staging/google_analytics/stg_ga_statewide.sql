b020740f42c7d835315fd44bda214e2272938e9e
Brittany Allen <brittany.allen@innovation.ca.gov>
2025-07-02 20:20:48 +0000

    removed division and function that were not needed

--- transform/models/staging/google_analytics/stg_ga_statewide.sql
+++ transform/models/staging/google_analytics/stg_ga_statewide.sql
@@ -7,7 +7,7 @@
 
 SELECT
     EVENT_DATE,
-    convert_timezone('UTC', 'America/Los_Angeles', to_timestamp(EVENT_TIMESTAMP / 1000))
+    convert_timezone('UTC', 'America/Los_Angeles', EVENT_TIMESTAMP)
         AS EVENT_TIMESTAMP_PST,
     EVENT_NAME,
     EVENT_PARAMS,
