644e446e157ac260e7208dd8df7c4f47d6c90a1a
Brittany Allen <brittany.allen@innovation.ca.gov>
2025-08-20 21:13:37 +0000

    review and ci changes

--- transform/models/marts/engaged_ca/ec__e3_total_users_and_page_views.sql
+++ transform/models/marts/engaged_ca/ec__e3_total_users_and_page_views.sql
@@ -2,15 +2,7 @@ with source_data as (
     select * from {{ ref('ga__total_users_and_page_views') }}
     where
         event_date >= '2025-08-01' -- soft launch date
-        and startswith(page_location, 'https://engaged.ca.gov/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/es/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/ko/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/tl/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/vi/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/zh-hans/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/zh-hant/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/fa/stateemployees/')
-        or startswith(page_location, 'https://engaged.ca.gov/hy/stateemployees/')
+        and page_location rlike $$^https://engaged.ca.gov(/[\w-]+)?/stateemployees/.*$$
 ),
 
 totals as (
