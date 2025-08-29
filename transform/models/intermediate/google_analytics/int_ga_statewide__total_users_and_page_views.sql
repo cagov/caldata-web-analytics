165964e69f3f3c217c77205bbd1eaaa940ff26bd
Brittany Allen <brittany.allen@innovation.ca.gov>
2025-08-29 17:17:18 +0000

    variable rename

--- transform/models/intermediate/google_analytics/int_ga_statewide__total_users_and_page_views.sql
+++ transform/models/intermediate/google_analytics/int_ga_statewide__total_users_and_page_views.sql
@@ -1,10 +1,10 @@
-{% set date = ga_statewide_beg_date if target.name == 'prd' else (modules.datetime.datetime.now() - modules.datetime.timedelta(days=7)).isoformat() %}
+{% set dt = ga_statewide_beg_date if target.name == 'prd' else (modules.datetime.datetime.now() - modules.datetime.timedelta(days=7)).isoformat() %}
 
 {{ config(
     materialized='incremental',
     incremental_strategy='microbatch',
     event_time='EVENT_DATE',
-    begin=date,
+    begin=dt,
     batch_size='day',
     snowflake_warehouse = get_snowflake_refresh_warehouse(big="XL", small="XS")
 ) }}
